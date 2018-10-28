b3d9ae91820b9df396fdb274aa12d4c4534cc5a5
Amir h Aghajani <amirhaghajani@gmail.com>
2018-10-28 15:09:07 +0330

    css

--- src/containers/myChatContainer.js
+++ src/containers/myChatContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Chat from '../components/Chat';
 import MyChatPeoples from '../components/MyChatPeoples';
-import { Link, IndexLink } from 'react-router';
+import { Link } from 'react-router';
 import {
   setCurrentUserID,
   addMessage,
