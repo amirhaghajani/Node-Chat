1e3356aacf8a882fe85aac5d43850e80255f7193
Amir h Aghajani <amirhaghajani@gmail.com>
2018-11-03 13:47:55 +0330

    زسس

--- src/containers/myChatContainer.js
+++ src/containers/myChatContainer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Chat from '../components/Chat';
 import MyChatPeoples from '../components/MyChatPeoples';
-import { Link } from 'react-router';
 import {
   setCurrentUserID,
   addMessage,
