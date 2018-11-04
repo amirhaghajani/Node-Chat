2eb318972746047add44916217ac9f3de03adcf6
Amir h Aghajani <amirhaghajani@gmail.com>
2018-11-04 08:34:26 +0330

    filter

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -3,7 +3,7 @@ const context = require('../models/context');
 module.exports.post = post;
 
 async function post(req, res) {
-  console.log('Request post: ' + req.body);
+  console.log('Request post: ' + JSON.stringify(req.body));
   switch (req.body.type) {
   case 'addUserToChatWiths':
     if (!req.session || !req.session.isAuthenticated) {
